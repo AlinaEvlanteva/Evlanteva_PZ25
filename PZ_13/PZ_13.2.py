51c3b85781755d32d023143192b6413f28a5e595
AlinaEvlanteva <evlantevaalina@icloud.com>
2024-02-24 10:38:43 +0300

    pz13

--- PZ_13/PZ_13.2.py
+++ PZ_13/PZ_13.2.py
@@ -1,4 +1,5 @@
 #В матрице найти среднее арифметическое положительных элементов, кратных 3
+from functools import reduce
 import random
 print("Исходная матрица:")
 matrix = [[random.randint(1, 10) for _ in range(3)] for _ in range(3)]
@@ -6,14 +7,27 @@ for row in matrix:
     print(row)
 def function (matrix):
     martix_in_spisok = [element for row in matrix for element in row]
-    filtered_elements = filter(lambda x: x > 0 and x % 3 == 0,  martix_in_spisok)  # Фильтруем положительные элементы, кратные 3
-    count = 0 # кол-во чисел
-    total = 0 #сумма чисел
-    for element in filtered_elements:
-        count += 1
-        total += element
-    if count > 0:
-        return total / count
-    else:
-        return 0
-print("Среднее арифметическое:", function(matrix))
+    filtered_elements = filter(lambda x: x > 0 and x % 3 == 0,  martix_in_spisok)
+    filtered_elements_list = list(filtered_elements)
+    sum_filtered = reduce(lambda x, y: x + y, filtered_elements_list)
+    count_filtered = len(filtered_elements_list)
+    return sum_filtered / count_filtered
+
+
+print('Среднее арифметическое:', function(matrix))
+
+
+
+
+
+#     count = 0
+#     sum = 0
+#     for element in filtered_elements:
+#         count += 1
+#         sum += element
+#     if count > 0:
+#         return sum / count
+#     else:
+#         return 0
+# print("Среднее арифметическое:", function(matrix))
+
