026950e89b568718903b57ea45bc1ada85e59b4f
AlinaEvlanteva <evlantevaalina@icloud.com>
2024-03-15 21:15:28 +0300

    pz14

--- PZ_14/PZ_14_1.py
+++ PZ_14/PZ_14_1.py
@@ -1,7 +1,7 @@
 #В исходном текстовом файле(Dostoevsky.txt) найти все варинаты фамилии Достоевского
 #(т.е с различными окончаниями, например , Достоевский, Достоевского) в единственном экзампляре.
 import re
-with open('Dostoevsky.txt', 'r', encoding='utf-8') as file:
+with open('PZ14/Dostoevsky.txt', 'r', encoding='utf-8') as file:
     content = file.read()
     print('Содержимое файла:',
           '\n',content)
